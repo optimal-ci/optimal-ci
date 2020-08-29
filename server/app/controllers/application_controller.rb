e65bc1ba24c76180460d26db804069783e793080
Mohsen Alizadeh <mohsen@alizadeh.us>
2020-08-29 18:30:27 +0430

    server: add simple authorization

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+  class NotAuthorized < Exception; end
 
+  before_action :authorize
+
+  def authorize
+    raise NotAuthorized if request.headers['AUTHORIZATION'] != ENV['OPTIMAL_CI_TOKEN']
+  end
+
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+  rescue_from NotAuthorized, with: :forbidden
 
   def unprocessable_entity
     render json: 'invalid parameters', status: :unprocessable_entity
+  end
+
+  def forbidden
+    render json: 'forbidden', status: :forbidden
   end
 end
