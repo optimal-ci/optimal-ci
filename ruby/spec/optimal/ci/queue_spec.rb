325b029daa7e5ec9fbdfb89f6b9a4d4614340918
Mohsen Alizadeh <mohsen@alizadeh.us>
2020-08-30 13:07:15 +0430

    ruby: fix ENV expectations

--- ruby/spec/optimal/ci/queue_spec.rb
+++ ruby/spec/optimal/ci/queue_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe Optimal::CI::Queue do
 
   describe "#push" do
     it 'sends files to the server' do
+      expect(ENV).to receive(:[]).twice.with('OPTIMAL_CI_TOKEN').and_return("something")
+      expect(ENV).to receive(:[]).twice.with('OPTIMAL_CI_URL').and_return("something")
+
+      expect(RestClient).to receive(:post).and_return(double(code: 204))
+
       expect(queue.push(['spec/mositala_spec.rb'])).to eq(true)
     end
   end
