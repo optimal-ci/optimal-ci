81c01deec6340777444eb312d5e69441ba52a0f7
Mohsen Alizadeh <mohsen@alizadeh.us>
2020-09-06 00:04:09 +0430

    implement server side

--- server/app/controllers/builds_controller.rb
+++ server/app/controllers/builds_controller.rb
@@ -3,14 +3,36 @@ class BuildsController < ApplicationController
     Build.transaction do
       return head :conflict if current_build(lock: true)
 
+      project_files = current_project.files
+      total_files = params[:total_files]
+
+      return unprocessable_entity unless total_files
+
+      files_with_runtime = []
+
+      total_files.each do |filename, mtime|
+        key = filename + "_" + mtime
+
+        time = if project_files[key]
+                 project_files[key].to_f
+               else
+                 9999
+               end
+
+        files_with_runtime << [filename, time]
+      end
+
+      sorted_files = files_with_runtime.sort_by { |filanem, time| -time }.map { |f| f[0] }
+
       build = Build.create!(
-        queue:              params[:total_files],
+        queue:              sorted_files,
         ci:                 params[:ci],
         build_number:       params[:build_number],
-        total_files_count:  params[:total_files].try(:count),
+        total_files_count:  sorted_files.count,
         project:            current_project,
         command:            params[:command_arguments_string]
       )
+
 
       params[:total_nodes].to_i.times do |i|
         Node.create!(index: i, build: build)
--- server/spec/requests/builds/create_spec.rb
+++ server/spec/requests/builds/create_spec.rb
@@ -1,12 +1,13 @@
 RSpec.describe "POST /builds", :type => :request do
-  let(:project) { create(:project) }
+  let(:project) { create(:project, files: { "file_1_#{time}" => "0.5" }) }
+  let(:time) { Time.now.to_i.to_s }
 
   def do_request(params  = {})
     post "/builds", params: params, headers: { Authorization: project.token }
   end
 
   it "creates a build" do
-    do_request(total_files: ['felan_spec.rb'], ci: 'travis', build_number: '1')
+    do_request(total_files: {'felan_spec.rb' => time.to_i, "file_1" => time.to_i }, ci: 'travis', build_number: '1')
     expect(response).to have_http_status(:no_content)
   end
 
@@ -15,7 +16,7 @@ RSpec.describe "POST /builds", :type => :request do
     expect(response).to have_http_status(:unprocessable_entity)
 
 
-    do_request(total_files: ['felan_spec.rb'], ci: 'travis')
+    do_request(total_files: {'felan_spec.rb' => time.to_i}, ci: 'travis')
     expect(response).to have_http_status(:unprocessable_entity)
 
     do_request(build_number: '1', ci: 'travis')
@@ -23,11 +24,11 @@ RSpec.describe "POST /builds", :type => :request do
   end
 
   it "does not create duplicated builds" do
-    do_request(total_files: ['felan_spec.rb'], ci: 'travis', build_number: '1')
+    do_request(total_files: {'felan_spec.rb' => time}, ci: 'travis', build_number: '1')
 
     expect(response).to have_http_status(:no_content)
 
-    do_request(total_files: ['felan_spec.rb'], ci: 'travis', build_number: '1')
+    do_request(total_files: {'felan_spec.rb' => time}, ci: 'travis', build_number: '1')
 
     expect(response).to have_http_status(:conflict)
   end
