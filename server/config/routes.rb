9aa6341eeff3a8cb5d678ab15d75fc17fb19c6ec
Mohsen Alizadeh <mohsen-alizadeh@users.noreply.github.com>
2020-09-05 18:13:48 +0430

    refactor get_one_file to queue/pop (#34)
    * refactor get_one_file to queue/pop
    
    * fix specs

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :builds, only: [:create]
 
-  get '/builds/:build_number/get_one_file' => 'builds#get_one_file'
+  get '/builds/:build_number/queue/pop' => 'builds#pop'
   patch '/builds/:build_number/report_duration' => 'builds#report_duration'
   patch '/builds/:build_number/report_http_calls' => 'builds#report_http_calls'
 end
