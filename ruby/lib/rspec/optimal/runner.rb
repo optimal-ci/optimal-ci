db018112bf3f01d472fbf2cceab834c34edfe338
Mohsen Alizadeh <mohsen-alizadeh@users.noreply.github.com>
2020-09-08 23:22:57 +0430

    support extra parameters (#56)
    * support spec parameters
    
    * fix formatter

--- ruby/lib/rspec/optimal/runner.rb
+++ ruby/lib/rspec/optimal/runner.rb
@@ -5,16 +5,26 @@ module RSpec
         'bundle exec rspec'
       end
 
-      def dir
-        'spec'
-      end
-
       def files_end_with
         "_spec.rb"
       end
 
+      def paths
+        @paths ||=
+          if @args.empty?
+            ['spec/']
+          else
+            ::RSpec::Core::Parser.parse(@args)[:files_or_directories_to_run]
+          end
+      end
+
+
+      def arguments
+        @args - @paths
+      end
+
       def run_examples(examples)
-        args = ['--format', 'RSpec::Optimal::Formatters::DocumentationFormatter'] + examples
+        args = ['--format', 'RSpec::Optimal::Formatters::DocumentationFormatter'] + arguments + examples
         RSpec::Core::Runner.run(args)
         RSpec.clear_examples
       end
